d43e57795ea4182c4fc4c5545aa1db5aa7eef453
Eliel Cezar <elielcezar@gmail.com>
2026-04-13 10:06:47 -0300

    fix: ajusta a porta para publicação em prod

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,19 +2,19 @@ module.exports = {
   apps: [{
     name: 'hajar-front',
     script: 'node_modules/next/dist/bin/next',
-    args: 'start -p 3008',
-    cwd: '/home/hajar-novo/htdocs/novo.hajar.com.br',
+    args: 'start -p 3012',
+    cwd: '/home/hajar/htdocs/hajar.com.br',
     instances: 1,
     exec_mode: 'cluster',
     watch: false,
     max_memory_restart: '1G',
     env: {
       NODE_ENV: 'production',
-      PORT: 3008
+      PORT: 3012
     },
-    error_file: '/home/hajar-novo/logs/hajar-front-error.log',
-    out_file: '/home/hajar-novo/logs/hajar-front-out.log',
-    log_file: '/home/hajar-novo/logs/hajar-front-combined.log',
+    error_file: '/home/hajar/logs/hajar-front-error.log',
+    out_file: '/home/hajar/logs/hajar-front-out.log',
+    log_file: '/home/hajar/logs/hajar-front-combined.log',
     time: true
   }]
 };
